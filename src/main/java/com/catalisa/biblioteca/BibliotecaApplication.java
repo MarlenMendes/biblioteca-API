65cbeefe5c054423ef8cede0e59c720a7b8b421f
MarlenMendes <marlen.mendes@zup.com.br>
2023-09-11 17:41:13 -0300

    Exclui segurança automatica

--- src/main/java/com/catalisa/biblioteca/BibliotecaApplication.java
+++ src/main/java/com/catalisa/biblioteca/BibliotecaApplication.java
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })	
 @OpenAPIDefinition(info = @Info(title = "Biblioteca API", version = "1", description = "Api para gerenciamento de biblioteca"))
 public class BibliotecaApplication {
 
